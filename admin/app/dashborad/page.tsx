c6074aefd4831b0bdd8ddc978eaa7ab60c21cb7a
thilinaruwan11 <ict20838@fot.sjp.ac.lk>
2025-05-29 01:54:19 +0530

    Update page.tsx

--- admin/app/dashborad/page.tsx
+++ admin/app/dashborad/page.tsx
@@ -1,10 +1,8 @@
 // app/dashboard/page.tsx
 "use client";
-import DashboardContent from '@/components/dashboard/dashboard';
+import DashboardContent from "@/components/dashboard/dashboard";
 
 export default function DashboardPage() {
-
-
   return (
     <div className="min-h-screen bg-gray-100">
       <DashboardContent />
